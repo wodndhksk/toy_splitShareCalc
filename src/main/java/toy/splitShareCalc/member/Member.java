3f2a55ea279988a93cbcfc1f7dfa7346da186674
Jaewoo Kim <wodndhksk@gmail.com>
2023-10-29 18:31:10 +0900

    add: User 추가 삭제 버튼 script 추가

---
+++ src/main/java/toy/splitShareCalc/member/Member.java
@@ -0,0 +1,27 @@
+package toy.splitShareCalc.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+}
